b42bc4e4cdbc4ec905def35ce735c231f5ee7667
Carlos Eduardo <carlos_eduardopr123@hotmail.com>
2024-06-12 21:24:55 -0300

    Funcionalidades da tela de Login implementadas

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,49 +1,64 @@
 import { Link } from "react-router-dom";
 import LoginCadastro from "../../components/LoginCadastro";
+import { useNavigate } from "react-router-dom";
 
 export default function Login() {
+  const navigate = useNavigate();
+
+  const handleEntrarClick = (event) => {
+    event.preventDefault();
+    const email = document.getElementById('exampleInputEmail1').value;
+    const senha = document.getElementById('exampleInputPassword1').value;
+    
+    if (email && senha) {
+      navigate("/inicio");
+    } else {
+      alert("Por favor, preencha todos os campos.");
+    }
+  };
+
   return (
     <main
-      class="p-5 d-flex flex-column justify-content-center align-items-center "
+      className="p-5 d-flex flex-column justify-content-center align-items-center "
       style={{ height: "80vh" }}
     >
       <LoginCadastro />
-      <form class="p-5">
-        <h1 class="pb-3">Login</h1>
-        <div class="mb-3">
-          <label for="exampleInputEmail1" class="form-label">
+      <form className="p-5">
+        <h1 className="pb-3">Login</h1>
+        <div className="mb-3">
+          <label htmlFor="exampleInputEmail1" className="form-label">
             Email
           </label>
           <input
             type="email"
-            class="form-control"
+            className="form-control"
             id="exampleInputEmail1"
             aria-describedby="emailHelp"
+            required
           />
         </div>
-        <div class="mb-3">
-          <label for="exampleInputPassword1" class="form-label">
+        <div className="mb-3">
+          <label htmlFor="exampleInputPassword1" className="form-label">
             Senha
           </label>
           <input
             type="password"
-            class="form-control"
+            className="form-control"
             id="exampleInputPassword1"
+            required
           />
         </div>
-        <div class="mb-3 form-check">
-          <input type="checkbox" class="form-check-input" id="exampleCheck1" />
-          <label class="form-check-label" for="exampleCheck1">
+        <div className="mb-3 form-check">
+          <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+          <label className="form-check-label" htmlFor="exampleCheck1">
             Lembrar-me
           </label>
         </div>
-        <button type="submit" class="btn btn-primary" style={{ width: "100%" }}>
+        <button type="submit" className="btn btn-primary" style={{ width: "100%" }} onClick={handleEntrarClick}>
           Entrar
         </button>
       </form>
-      <p>
-        Não tem uma conta? <Link to="/register">Registre-se aqui</Link>
-      </p>
+      Não tem uma conta? <Link to="/register">Registre-se aqui</Link>
     </main>
   );
 }
